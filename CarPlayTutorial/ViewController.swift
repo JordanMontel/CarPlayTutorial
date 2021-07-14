7beeff92cc4fa448931164ea05059251c52709ec
Jordan Montel <jordan.montel@gmail.com>
2021-07-14 11:31:06 +0200

    Create a first Car Play App

---
+++ CarPlayTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarPlayTutorial
+//
+//  Created by Jordan Montel on 14/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
